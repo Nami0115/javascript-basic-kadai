a36d775ff8a50ef2c1291438a7299c0bae3b052f
Nami0115 <lifeihave156@gmail.com>
2024-07-19 22:34:57 +0900

    1回目の修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date ();
 const year = today.getFullYear() + '年';
-const month = today.getMonth () + '月';
+const month = today.getMonth () + 1 + '月';
 const day = today.getDate () + '日';
 
 console.log(year + month + day);
